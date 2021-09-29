a65b7d83fa7ebbd4b2bd7eb908cc171d4c007291
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-09-29 10:20:52 +0100

    Add server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, Now has imported new script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('body');
 
     return element;
 }
